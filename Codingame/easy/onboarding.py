323b96c28ec7b4fea40a77187d3543ad1672bff9
migafgarcia <migafgarcia@gmail.com>
2016-10-03 21:15:41 +0100

    Added onboarding.py

---
+++ Codingame/easy/onboarding.py
@@ -0,0 +1,22 @@
+import sys
+import math
+
+# CodinGame planet is being attacked by slimy insectoid aliens.
+# <---
+# Hint:To protect the planet, you can implement the pseudo-code provided in the statement, below the player.
+
+
+# game loop
+while 1:
+    enemy_1 = raw_input() # name of enemy 1
+    dist_1 = int(raw_input()) # distance to enemy 1
+    enemy_2 = raw_input() # name of enemy 2
+    dist_2 = int(raw_input()) # distance to enemy 2
+
+    # Write an action using print
+    # To debug: print >> sys.stderr, "Debug messages..."
+
+    if dist_1 < dist_2:
+        print enemy_1
+    else:
+        print enemy_2

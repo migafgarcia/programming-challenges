bde9ac5db5d31bbfeedda1f6d7a480930f71991a
migafgarcia <migafgarcia@gmail.com>
2017-07-01 18:50:38 +0100

    Update ratatouille.py
    NOT YET SOLVED
    New approach using bounds of servings.
    Only the submission with small dataset is accepted

--- google_code_jam/round_1a_2017/ratatouille/ratatouille.py
+++ google_code_jam/round_1a_2017/ratatouille/ratatouille.py
@@ -3,6 +3,25 @@
 # https://code.google.com/codejam/contest/5304486/dashboard
 
 from __future__ import print_function
+from __future__ import division
+import math
+from operator import itemgetter
+
+def get_bounds(r, p):
+	"""
+		r -> recipe
+		p -> quantity in package
+
+	"""
+
+	lower = (p * 10) / (r * 11)
+	
+	upper = (p * 10) / (r * 9)
+
+	return (math.ceil(lower), math.floor(upper))
+
+def bounds_overlap(b1, b2):
+	return not (b2[1] < b1[0] or b1[1] < b2[0])
 
 t = int(raw_input())
 
@@ -15,49 +34,34 @@ for case in range(t):
 	for _ in range(n):
 		q.append([int(x) for x in raw_input().split()])
 
-
-
-	
 	c = 0
+
+	bounds = [[tuple() for x in range(p)] for y in range(n)]
+
 	for package in range(p):
-		# print('package = ', end = '')
-		# print(package)
-		servings = list()
-		flag = False
 		for ingredient in range(n):
-			# print('ingredient = ', end = '')
-			# print(ingredient)
-			if ingredient == 0:
-				for k in range(51):
-					if q[ingredient][package] >= r[ingredient] * k * 0.9 and q[ingredient][package] <= r[ingredient] * k * 1.1:
-						# print(k, end = '')
-						# print(" servings")
-						# print(r[ingredient] * k * 0.9, end = '')
-						# print(" <= ", end = '')
-						# print(q[ingredient][package], end = '')
-						# print(" <= ", end = '')
-						# print(r[ingredient] * k * 1.1)
-						servings.append(k)
-						flag = True
-				if len(servings) == 0: break
-			else:
-				servings_flag = False
-				for s in servings:
-					if q[ingredient][package] >= r[ingredient] * s * 0.9 and q[ingredient][package] <= r[ingredient] * s * 1.1:
-						# print(servings, end = '')
-						# print(" servings")
-						# print(r[ingredient] * s * 0.9, end = '')
-						# print(" <= ", end = '')
-						# print(q[ingredient][package], end = '')
-						# print(" <= ", end = '')
-						# print(r[ingredient] * s * 1.1)
-						servings_flag = True
-				flag = servings_flag
+			current = get_bounds(r[ingredient], q[ingredient][package])
+
+			if current[0] <= current[1]:
+				bounds[ingredient][package] = (current[0], current[1], False)
+
+	# without binary search
+	for bound in bounds[0]:
+		if len(bound) == 0:
+			continue
+		flag = True
+		for ingredient in range(1, n):
+			inner_flag = False
+			for package in range(p):
+				if len(bounds[ingredient][package]) == 0 or bounds[ingredient][package][2]:
+					continue
+				if bounds_overlap(bound, bounds[ingredient][package]):
+					inner_flag = True
+					break
+			if not inner_flag:
+				flag = False
+				break
 		if flag:
-			c +=1
-
-
-
-			
+			c += 1
 
 	print(c)

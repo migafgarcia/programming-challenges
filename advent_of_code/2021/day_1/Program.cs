25cbab2b7acb9e37ea53345bc1e9a088b96f6fa8
Miguel Garcia <MiguelGarcia@criticalmanufacturing.com>
2021-12-06 22:10:30 +0000

    AoC 2021 Day 1 & 2

---
+++ advent_of_code/2021/day_1/Program.cs
@@ -0,0 +1,48 @@
+﻿
+var input = await File.ReadAllLinesAsync(args[0]);
+
+#region Part One
+
+var count = 0;
+int? previousValue = null;
+
+foreach(var line in input)
+{
+    var lineInt = int.Parse(line);
+    if (previousValue.HasValue && lineInt > previousValue)
+    {
+        count++;
+    }
+    previousValue = lineInt;
+}
+
+Console.WriteLine(count);
+
+#endregion
+
+#region Part Two
+
+int slidingWindowSize = 3;
+
+int[] slidingWindow = new int[slidingWindowSize];
+
+int previousSum = 0;
+int slidingCounter = 0;
+
+for(int i = 0; i < input.Length; i++)
+{
+
+    int lineInt = int.Parse(input[i]);
+
+    slidingWindow[i % slidingWindowSize] = lineInt;
+    var currentSum = slidingWindow.Sum();
+
+    if (i >= slidingWindowSize && currentSum > previousSum)
+    {
+        slidingCounter++;
+    }
+
+    previousSum = currentSum;
+}
+Console.WriteLine(slidingCounter);
+#endregion

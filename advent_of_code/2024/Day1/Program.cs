0efe7ba3386a26b906a01a04ff58fed4d8b28c3d
magarcia <migafgarcia@gmail.com>
2024-12-01 15:07:51 +0000

    Add day 1

---
+++ advent_of_code/2024/Day1/Program.cs
@@ -0,0 +1,70 @@
+﻿
+var input = await File.ReadAllLinesAsync(@"day1part1.txt");
+
+Part1(input);
+Part2(input);
+return;
+
+void Part1(string[] strings)
+{
+    
+    var leftList = new int[strings.Length];
+    var rightList = new int[strings.Length];
+
+    for (var i = 0; i < strings.Length; i++)
+    {
+        var entry = strings[i].Split("   ");
+        leftList[i] = int.Parse(entry[0]);
+        rightList[i] = int.Parse(entry[1]);
+    }
+
+    Array.Sort(leftList);
+    Array.Sort(rightList);
+
+    var sum = 0;
+
+    for (var i = 0; i < strings.Length; i++)
+    {
+        sum += Math.Abs(rightList[i] - leftList[i]);
+    }
+
+    Console.WriteLine(sum);
+}
+
+void Part2(string[] strings)
+{
+    var leftList = new Dictionary<int, int>();
+    var rightList = new Dictionary<int, int>();
+
+    foreach (var t in strings)
+    {
+        var entry = t.Split("   ");
+        var leftItem = int.Parse(entry[0]);
+        var rightItem = int.Parse(entry[1]);
+
+        if(!leftList.TryAdd(leftItem, 1))
+        {
+            leftList[leftItem]++;
+        }
+
+        if(!rightList.TryAdd(rightItem, 1))
+        {
+            rightList[rightItem]++;
+        }
+    }
+
+    var score = 0;
+    foreach (var (leftItem, leftCount) in leftList)
+    {
+        if (!rightList.TryGetValue(leftItem, out var rightCount))
+        {
+            continue;
+        }
+
+        score += (leftItem * rightCount) * leftCount;
+    }
+    
+    Console.WriteLine(score);
+
+}
+

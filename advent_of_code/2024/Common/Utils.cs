4af1ea611108070907bc4fcbb487a2b96f16399b
magarcia <migafgarcia@gmail.com>
2024-12-23 11:08:18 +0000

    Add day 7

--- advent_of_code/2024/Common/Utils.cs
+++ advent_of_code/2024/Common/Utils.cs
@@ -30,8 +30,28 @@ public static class Utils
 
     }
     
+    //
+    // public static void RunPuzzle(Func<string[], int> puzzleFunc, string[] puzzleInput, Action<int> testAction)
+    // {
+    //     
+    //     var stopwatch = new Stopwatch();
+    //
+    //     stopwatch.Start();
+    //
+    //     var result = puzzleFunc(puzzleInput);
+    //
+    //     stopwatch.Stop();
+    //
+    //     var elapsed = stopwatch.Elapsed;
+    //
+    //     testAction.Invoke(result);
+    //     
+    //     Console.WriteLine($"[{Assembly.GetCallingAssembly().GetName().Name}] Result: {result}, Elapsed time: {elapsed.TotalMilliseconds} ms");
+    //
+    // }
     
-    public static void RunPuzzle(Func<string[], int> puzzleFunc, string[] puzzleInput, Action<int> testAction)
+    
+    public static void RunPuzzle<T>(Func<string[], T> puzzleFunc, string[] puzzleInput, Action<T> testAction)
     {
         
         var stopwatch = new Stopwatch();
@@ -49,6 +69,7 @@ public static class Utils
         Console.WriteLine($"[{Assembly.GetCallingAssembly().GetName().Name}] Result: {result}, Elapsed time: {elapsed.TotalMilliseconds} ms");
 
     }
+
 
     
 }
--- advent_of_code/2024/Day7/Program.cs
+++ advent_of_code/2024/Day7/Program.cs
@@ -1,2 +1,102 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+using Common;
+using FluentAssertions;
+
+var rawTestInput = await File.ReadAllLinesAsync("input.test");
+var rawInput = await File.ReadAllLinesAsync("input.txt");
+//
+Utils.RunPuzzle(Part1, rawTestInput, result =>
+{
+    result.Should().Be(3749);
+});
+Utils.RunPuzzle(Part1, rawInput, result =>
+{
+    result.Should().Be(12940396350192);
+});
+
+Utils.RunPuzzle(Part2, rawTestInput, result =>
+{
+    result.Should().Be(11387);
+});
+Utils.RunPuzzle(Part2, rawInput, result =>
+{
+    result.Should().Be(106016735664498);
+});
+
+return;
+
+long Part1(string[] strings)
+{
+
+    long totalCalibrationResult = 0;
+    foreach (var line in strings)
+    {
+        var splitted = line.Split(":");
+        var calibrationResult = long.Parse(splitted[0]);
+        
+        var operators = 
+            Regex.Matches(splitted[1], @"\d+")
+                .Select(match => long.Parse(match.Value))
+                .ToArray();
+        
+        if (ProcessOperators(calibrationResult, operators[0], operators, 1))
+        {
+            totalCalibrationResult += calibrationResult;
+        }
+    }
+
+    return totalCalibrationResult;
+
+}
+
+bool ProcessOperators(long expectedResult, long currentResult, long[] operatorsToProcess, int index)
+{
+    
+    if (index == operatorsToProcess.Length)
+    {
+        return expectedResult == currentResult;
+    }
+
+    var currentOperator = operatorsToProcess[index];
+
+    return ProcessOperators(expectedResult, currentResult + currentOperator, operatorsToProcess, index + 1) || 
+           ProcessOperators(expectedResult, currentResult * currentOperator, operatorsToProcess, index + 1);
+}
+
+bool ProcessOperatorsElephants(long expectedResult, long currentResult, long[] operatorsToProcess, int index)
+{
+    
+    if (index == operatorsToProcess.Length)
+    {
+        return expectedResult == currentResult;
+    }
+
+    var currentOperator = operatorsToProcess[index];
+    
+    return ProcessOperatorsElephants(expectedResult, long.Parse($"{currentResult}{currentOperator}"), operatorsToProcess, index + 1) || 
+           ProcessOperatorsElephants(expectedResult, currentResult + currentOperator, operatorsToProcess, index + 1) || 
+           ProcessOperatorsElephants(expectedResult, currentResult * currentOperator, operatorsToProcess, index + 1);
+}
+
+long Part2(string[] strings)
+{
+    long totalCalibrationResult = 0;
+    foreach (var line in strings)
+    {
+        var splitted = line.Split(":");
+        var calibrationResult = long.Parse(splitted[0]);
+        
+        var operators = 
+            Regex.Matches(splitted[1], @"\d+")
+                .Select(match => long.Parse(match.Value))
+                .ToArray();
+        
+        if (ProcessOperatorsElephants(calibrationResult, operators[0], operators, 1))
+        {
+            totalCalibrationResult += calibrationResult;
+        }
+    }
+
+    return totalCalibrationResult;
+
+}

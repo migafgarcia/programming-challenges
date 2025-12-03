79a691e4069cc100a8da73844eb68542aa84f76f
magarcia <migafgarcia@gmail.com>
2025-12-03 00:44:33 +0000

    Add AoC 2025 Day 2 Part 1

---
+++ advent_of_code/2025/aoc/src/day2.rs
@@ -0,0 +1,55 @@
+use nom::{IResult, Parser};
+use nom::multi::separated_list0;
+use nom::bytes::complete::{tag};
+use nom::character::complete::digit1;
+use nom::combinator::{map_res, recognize};
+use nom::sequence::separated_pair;
+
+fn num_len(n: u64) -> u32 {
+    (n as f32).log10().floor() as u32 + 1
+}
+
+fn input_parser(s: &str) -> IResult<&str, Vec<(u64, u64)>> {
+    separated_list0(tag(","), range_parser).parse(s)
+}
+
+fn range_parser(s: &str) -> IResult<&str, (u64, u64)> {
+    separated_pair(digit_parser, tag("-"), digit_parser).parse(s)
+}
+
+fn digit_parser(s: &str) -> IResult<&str, u64> {
+    map_res(recognize(digit1), str::parse).parse(s)
+}
+
+pub fn day2part1(s: &str) -> u64 {
+
+    let (_, ranges) = input_parser(s).unwrap();
+    let mut result: u64 = 0;
+
+    for (from, to) in ranges {
+        for num in from..=to {
+
+            let len = num_len(num);
+
+            // check if number is divisible by 2
+            if len % 2 != 0 {
+                continue;
+            }
+
+            let half_len = len / 2;
+
+            let first_half = num / 10_u64.pow(half_len);
+            let second_half = num % 10_u64.pow(half_len);
+
+            if first_half == second_half {
+                result += num;
+            }
+        }
+    }
+    result
+}
+
+
+pub fn day2part2(s: &str) -> u64 {
+    todo!()
+}
--- advent_of_code/2025/aoc/src/main.rs
+++ advent_of_code/2025/aoc/src/main.rs
@@ -1,30 +1,21 @@
-mod day1;
+mod day2;
 
 use std::fs;
 
 fn main() {
-    let s = "L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82";
+    let test_input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
-    assert_eq!(day1::day1part1(s), 3);
+    assert_eq!(day2::day2part1(test_input), 1227775554);
 
-    let contents = fs::read_to_string(r"input.txt").unwrap();
+    let contents = fs::read_to_string(r"C:\Users\magarcia\Documents\GitHub\programming-challenges\advent_of_code\2025\aoc\src\input.txt").unwrap();
 
-    let result_part1 = day1::day1part1(contents.as_str());
+    let result_part1 = day2::day2part1(contents.as_str());
 
-    println!("{}", result_part1);
+    assert_eq!(result_part1, 23701357374);
 
-    assert_eq!(day1::day1part2(s), 6);
+    assert_eq!(day2::day2part2(test_input), 4174379265);
 
-    let result_part2 = day1::day1part2(contents.as_str());
+    let result_part2 = day2::day2part2(contents.as_str());
 
     println!("{}", result_part2);
 }
